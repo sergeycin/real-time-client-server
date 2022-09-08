60b20e16084b4ce81fee4b0f717f96bc15420ec6
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-09-08 15:47:53 +0300

    package

--- client/src/longPulling.js
+++ client/src/longPulling.js
@@ -1,16 +1,41 @@
 import './App.css';
-import useState from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function LongPulling() {
   const [messages,setMessages] = useState([])
   const [value,setValue] = useState('')
-  
+
+  const sendMessage = async () =>{
+    await axios.post('http://localhost:5000/new-messages',{
+      message:value,
+      id: Date.now()
+    })
+  }
+
+  useEffect(()=>{
+    subscribe()
+  },[])
+
+  const subscribe = async () =>{
+    try{
+    const {data}  = await axios.get('http://localhost:5000/get-messages')
+    console.log(data)
+    setMessages(prev =>[data,...prev])
+    await subscribe()
+    }catch(e){
+      setTimeout(()=>{
+        subscribe()
+      },500)
+    }
+  }
+
   return (
     <div className="center">
         <div>
             <div className="form">
-                <input type="text" name="" id="" />
-                <button>Отправить</button>
+                <input value={value} onChange={e => setValue(e.target.value)} type="text" name="" id="" />
+                <button onClick={sendMessage}>Отправить</button>
             </div>
             <div className="messages">
                 {messages.map(mess =>
--- server/longpulling.js
+++ server/longpulling.js
@@ -5,15 +5,15 @@ const PORT  =  5000
 const app = express()
 
 const emitter = new events.EventEmitter()
-
-
-app.get('get-message', (req,res) => {
+app.use(cors())
+app.use(express.json())
+app.get('/get-messages', (req,res) => {
     emitter.once('newMessage', (message) =>{
         res.json(message)
     })
 })
 
-app.post('new-message', (req,res) => {
+app.post('/new-messages', (req,res) => {
     const message = req.body
     emitter.emit('newMessage',message)
     res.status(200)
